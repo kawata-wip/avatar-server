92d401d60217060a7c36faa02de7a31337f7bd29
Enzo <me@r33.space>
2020-11-15 19:46:25 +0100

    Revert "."
    This reverts commit 4f3303dee04c2e494b57a4d9dfb983ed68e011c5.

--- avatarserver.py
+++ avatarserver.py
@@ -16,29 +16,48 @@ curryear = int(datetime.now().year)
 today = datetime.date(datetime(curryear, int(datetime.now().month), int(datetime.now().day)))
 peppyday = datetime.date(datetime(curryear, 4, 20))
 
+@app.route("/status")
+def serverStatus():
+	return jsonify({
+		"response" : 200,
+		"status" : 1
+	})
+
 @app.route("/<int:uid>")
 def serveAvatar(uid):
 	# Check if avatar exists
 	peppy = random.randrange(1,16)
+	if peppyday != today:
+		if os.path.isfile("{}/{}.png".format(avatar_dir, uid)):
+			avatarid = uid
+		else:
+			avatarid = -1
+
+	returnthi = ""
 	if peppyday == today:
-		return send_file("{}/{}.png".format(peppy_dir, peppy))
-
-	if os.path.isfile("{}/{}.png".format(avatar_dir, uid)):
-		avatarid = uid
-	else:
-		avatarid = -1
-
+		returnthi = send_file("{}/{}.png".format(peppy_dir, peppy))
+	elif peppyday != today:
+		returnthi = send_file("{}/{}.png".format(avatar_dir, avatarid))
 	# Serve actual avatar or default one
-	return send_file("{}/{}.png".format(avatar_dir, avatarid))
+	return returnthi
+
+@app.route("/<int:uid>/peppyday")
+def serveAvatarPPYday(uid):
+	# Check if avatar exists
+	peppy = random.randrange(1,16)
+	
+	returnthi = send_file("{}/{}.png".format(peppy_dir, peppy))
+	# Serve actual avatar or default one
+	return returnthi
 
 @app.errorhandler(404)
 def page_not_found(error):
 	peppy = random.randrange(1,16)
 	if peppyday == today:
 		return send_file("{}/{}.png".format(peppy_dir, peppy))
-
-	return send_file("{}/-1.png".format(avatar_dir))
+	elif peppyday != today:
+		return send_file("{}/-1.png".format(avatar_dir))
 
 	
 # Run the server
-app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", debug=True, port=5000)
